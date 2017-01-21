8e0a3fbfbafe41e807f2fc83b5424ec91e325513
Emanuel Evans <mail@emanuel.industries>
2017-01-20 17:27:22 -0800

    Switch from grub to systemd-boot

--- configuration.nix
+++ configuration.nix
@@ -12,8 +12,8 @@
       ./machine-configuration.nix
     ];
 
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "zfs" ];
   boot.tmpOnTmpfs = true;
 
