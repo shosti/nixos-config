7442b35956861ade19b65976d1924ec6ec0fcc0c
Emanuel Evans <mail@emanuel.industries>
2017-02-20 20:09:22 -0800

    Oops, also gotta allow local connections

--- configuration.nix
+++ configuration.nix
@@ -127,6 +127,7 @@
         enable = true;
         package = pkgs.postgresql96;
         authentication = pkgs.lib.mkForce ''
+          local all all trust
           host all all 127.0.0.1/32 trust
         '';
       };
