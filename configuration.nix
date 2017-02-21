d9be28a17c32aa4298919eef5e77bfbdcdb1934e
Emanuel Evans <mail@emanuel.industries>
2017-02-20 18:31:51 -0800

    Easy config testing with VMs
    Also enable SSH, which should be OK I think.

--- configuration.nix
+++ configuration.nix
@@ -104,6 +104,10 @@
   users.users.shosti = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
+    # Useful for testing
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCoE5MAy3Sjt3taf14lJyh6T/qe21I/VzYqgcuiaDacLTa5cRyT/+qt6wtTb3UsD6I7zTtHuzr1klshSB/5vHP7LcZkr0P398ArOFV7MSv/sR2ZPX+9bbzL5Rlewqly4Ft+COdkGeWAWk32EeXyqGbLZVWUqSagatSa2YCWuT5FAFalbVg27nlbsXhVOTi0vDd2E33shJuVwOjq+HNA48ZMZXohLaTkxB+3dWZ1XfMcuyjkS/epHUvQeBGXff/Ox8EdIVXcfDtWL41N6GgkA0v+LAiGC84bxqOuGS97t3FMGUHodVIUSLZwblhT2M4P1h7IQa0N//QmSmwabO3newZZ shosti@themountain"
+    ];
   };
 
   virtualisation.virtualbox.host.enable = true;
@@ -150,6 +154,11 @@
   services.upower.enable = true;
 
   services.cron.enable = true;
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "no";
+    passwordAuthentication = false;
+  };
 
   security.sudo.wheelNeedsPassword = false;
 
