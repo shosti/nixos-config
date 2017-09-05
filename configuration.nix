763f26deb0624f2ab30434121ef49c676195a1e5
Emanuel Evans <mail@emanuel.industries>
2017-09-05 12:32:40 -0700

    Oops

--- configuration.nix
+++ configuration.nix
@@ -251,7 +251,7 @@
     KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0113|0114|0115|0116|0120|0200|0402|0403|0406|0407|0410", TAG+="uaccess"
 
     LABEL="u2f_end"
-  ''
+  '';
 
   # Some ngnix stuff for work...
   networking.extraHosts = ''
