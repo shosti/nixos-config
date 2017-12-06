c6315611a9f32614f4d1d39b5b83dd670722e5c8
Emanuel Evans <mail@emanuel.industries>
2017-12-05 18:48:56 -0800

    Update linux to 4.14

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems = [ "zfs" ];
   boot.tmpOnTmpfs = true;
-  boot.kernelPackages = pkgs.linuxPackages_4_13;
+  boot.kernelPackages = pkgs.linuxPackages_4_14;
 
   boot.zfs = {
     forceImportAll = false;
